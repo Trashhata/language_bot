fbb598d7a8003be765e0a9946db7c02e2e928c93
Maxim Popov <macs.wow@mail.ru>
2023-09-04 14:02:14 +0300

    Bug fix.

--- dictionaries/get_dictionary.py
+++ dictionaries/get_dictionary.py
@@ -10,4 +10,4 @@ def open_file_in_same_directory(file_name: str) -> str:
 def get_dictionary(language: str = 'en-ru'):
     # commands: dict[str, str] = {'en-ru': '.'}
 
-    return open(open_file_in_same_directory('eng_dictionary.csv'), 'r', encoding='utf-8')
+    return open(open_file_in_same_directory('eng_dictionary.csv'), 'r', encoding='windows-1251')
