f256029327fe55c59b1ad6d54489220a7e182083
Maxim Popov <macs.wow@mail.ru>
2023-09-04 13:57:09 +0300

    Bug fix.

--- external_services/get_random_word.py
+++ external_services/get_random_word.py
@@ -17,8 +17,16 @@ def get_new_words(amount: int, origin: str = 'en', res: str = 'ru') -> list[Word
     # csv file with dictionary
     dictionary = get_dictionary(language=f'{origin}-{res}')
 
-    reader = csv.reader(dictionary)
-    res = sample(list(reader), amount)
-    dictionary.close()
+    try:
+        reader = csv.reader(dictionary)
+        res = sample(list(reader), amount)
+        dictionary.close()
+
+    except UnicodeDecodeError:
+        dictionary.encoding = 'windows-1251'
+
+        reader = csv.reader(dictionary)
+        res = sample(list(reader), amount)
+        dictionary.close()
 
     return [Word(*i) for i in res]
